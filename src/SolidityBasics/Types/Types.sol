a73c27d2cdede3e613065a71805351bf81790c99
Genesis3800 <priyankgupta0308@gmail.com>
2023-08-29 09:40:31 +0530

    Added code for SolidityBasics/Types

---
+++ src/SolidityBasics/Types/Types.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract ValueType_vs_ReferenceType {
+
+    //Value Type
+    uint public valueVar1 = 10;
+    
+    // Reference Type: array of unsigned integers
+    uint[] public referenceVar1 = [1, 2, 3];
+
+    function modifyValueType(uint newValue) public view {
+        
+        // New Variable assigned the value of valueVar1
+        uint valueVar2 = valueVar1;  
+
+        //New variable passed 
+        valueVar2 = newValue;
+    }
+
+    function modifyReferenceType(uint index, uint newValue) public {
+        
+        // New variable, referenceVar2, refers to the same storage location as referenceVar1
+        uint[] storage referenceVar2 = referenceVar1;  
+        
+        // Modifying the localReference will modify referenceVar
+        referenceVar2[index] = newValue;              
+    }
+
+}
---
+++ src/SolidityBasics/Types/Types.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Test, console2} from "forge-std/Test.sol";
+import "./Types.sol";
+
+contract Types_test is Test {
+
+    ValueType_vs_ReferenceType types;
+
+    function setUp() public {
+
+        types = new ValueType_vs_ReferenceType();
+    }
+
+    function test_modifyValueType() public {
+
+        types.modifyValueType(1234);
+
+        // Check that the value of valueVar1 is still 10
+        assertEq(types.valueVar1(), 10);
+    }
+
+    function test_modifyReferenceType() public {
+
+        console2.log("The original value of referenceVar1[0] is", types.referenceVar1(0));
+        types.modifyReferenceType(0, 1234);
+
+        // Check that the value of referenceVar1[0] is now 1234
+        assertEq(types.referenceVar1(0), 1234);
+    }
+}
