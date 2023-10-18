4e7c813f88abbd55a69fb471805ef879f0614059
Genesis3800 <priyankgupta0308@gmail.com>
2023-10-19 00:37:59 +0530

    Incomplete LZ OFT tutorial

---
+++ src/Applications/LayerZero/Lz_OFT/OFT_Mumbai.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@layerzero-contracts/token/oft/v2/OFTV2.sol";
+
+contract OFT_Mumbai is OFTV2 {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _layerZeroEndpoint,
+        uint _initialSupply,
+        uint8 _sharedDecimals
+    ) OFTV2(_name, _symbol, _sharedDecimals, _layerZeroEndpoint) {
+        _mint(_msgSender(), _initialSupply);
+    }
+}
